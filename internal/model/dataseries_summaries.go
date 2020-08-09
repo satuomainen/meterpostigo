d7f983b45ecb60bc2af82e3afb99eae61bd501ef
Sami Tuomainen <satuomainen@gmail.com>
2020-08-09 17:15:34 +0300

    Fix DB column mapping

--- internal/model/dataseries_summaries.go
+++ internal/model/dataseries_summaries.go
@@ -6,9 +6,9 @@ type DataSeriesSummaries struct {
 	ID           uint64    `gorm:"column:id;primaryKey;not null;type:int(10);autoIncrement"`
 	CreatedAt    time.Time `gorm:"column:created_at;not null;autoCreateTime;default:current_timestamp()"`
 	UpdatedAt    time.Time `gorm:"column:updated_at;not null;autoCreateTime;default:current_timestamp()"`
-	CurrentValue string    `gorm:"column:value;type:varchar(255);not null"`
-	MinValue     string    `gorm:"column:value;type:varchar(255);not null"`
-	MaxValue     string    `gorm:"column:value;type:varchar(255);not null"`
+	CurrentValue string    `gorm:"column:current_value;type:varchar(255);not null"`
+	MinValue     string    `gorm:"column:min_value;type:varchar(255);not null"`
+	MaxValue     string    `gorm:"column:max_value;type:varchar(255);not null"`
 	DataSeriesID uint64    `gorm:"column:dataseries_id;type:int(10);not null;"`
 }
 
